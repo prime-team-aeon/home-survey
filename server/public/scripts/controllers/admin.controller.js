41728677b555c972af3466f885d911266952be8f
Adam Biessener <abiessener@gmail.com>
2017-09-28 08:22:30 -0500

    file chooser getting data, not sending it anywhere yet

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -1,4 +1,26 @@
 myApp.controller('AdminController', function() {
   console.log('AdminController created');
   var self = this;
+
+  self.uploadCsv = function(){
+    console.log('ac.uploadCsv()');       
+  }
+
+  function handleFileSelect(fileEvent){
+    console.log('hfs');
+    
+    reader = new FileReader();
+    reader.onerror = function(){
+      console.log('reader error');
+    };
+    reader.onload = function(readerEvent){
+      console.log('reader onload', readerEvent);
+      // this is where we actually send the data onward
+
+      // CsvService.uploadCsv(readerEvent.target.result);
+    }
+    reader.readAsText(fileEvent.target.files[0]);
+  }
+
+  document.getElementById('admin-file-input').addEventListener('change', handleFileSelect, false);
 });
