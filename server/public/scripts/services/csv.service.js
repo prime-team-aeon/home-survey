910fa8a3cb6595aa0bc4e168608e2b96ca8695be
Adam Biessener <abiessener@gmail.com>
2017-10-01 09:11:50 -0500

    minor code formatting

--- server/public/scripts/services/csv.service.js
+++ server/public/scripts/services/csv.service.js
@@ -1,18 +1,18 @@
-myApp.service('CsvService', function($http, $location, $mdToast){
+myApp.service('CsvService', function ($http, $location, $mdToast) {
   console.log('CsvService Loaded');
 
   var self = this;
 
   // called ultimately by the [UPLOAD] button on admin.html. Parses the imported file and sends it up to the server.
-  self.uploadCsv = function(file, year){
+  self.uploadCsv = function (file, year) {
     // thanks Papa!
     var parsed = Papa.parse(file);
 
     for (var i = 0; i < parsed.data.length; i++) {
       parsed.data[i].push(year);
     }
-    
-    $http.post('/csv/upload', parsed).then(function(response){
+
+    $http.post('/csv/upload', parsed).then(function (response) {
       $mdToast.show(
         $mdToast.simple()
         .textContent('CSV uploaded!')
@@ -21,24 +21,18 @@ myApp.service('CsvService', function($http, $location, $mdToast){
       $location.path('/admin');
     })
   }
-  
-  self.exportAllResponses = function(year){
-    $http.get('/csv/export/' + year).then(function(response){
 
-      // reformat data into client's preferred structure
-      // each row is a single survey, columns are answers
+  self.exportAllResponses = function (year) {
+    $http.get('/csv/export/' + year).then(function (response) {
+      // console.log('response.data', response.data);
 
-      console.log('response.data', response.data);
-      
       // format the data into a csv file
       var exportCsv = Papa.unparse(response.data);
       exportCsv = "data:text/csv;charset=utf-8," + exportCsv;
-      exportCsv = encodeURI(exportCsv);      
+      exportCsv = encodeURI(exportCsv);
 
       // tell the browser to download it
-      
-      // DEBUG -- UNCOMMENT NEXT LINE
-      // window.open(exportCsv);
+      window.open(exportCsv);
     })
   }
 });
