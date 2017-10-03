a1437fd88d6c6c2df501a7f53039c5d342582393
Sam Fortin <sam.j.fortin@gmail.com>
2017-10-03 10:47:28 -0500

    language specific get for translation table

--- server/routes/survey.router.js
+++ server/routes/survey.router.js
@@ -7,27 +7,29 @@ var pool = require('../modules/pool.js');
 
 router.get('/language', function (req, res) {
     console.log(req.query.language);
-    var language = req.query.language
+    var language = req.query.language;
+    var translation;
     switch (language) {
         case 'english':
-            language = "SELECT question_number, english FROM questions WHERE question_number BETWEEN 1 AND 27;"
+            language = "SELECT question_number, english FROM questions WHERE question_number BETWEEN 1 AND 27;";
+            translation = 'SELECT type, english FROM translations;';
             break;
         case 'somali':
             language = "SELECT question_number, somali FROM questions WHERE question_number BETWEEN 1 AND 27;"
+            translation = 'SELECT type, somali FROM translations;';
             break;
         case 'spanish':
             language = "SELECT question_number, spanish FROM questions WHERE question_number BETWEEN 1 AND 27;"
+            translation = 'SELECT type, spanish FROM translations;';
             break;
         case 'hmong':
             language = "SELECT question_number, hmong FROM questions WHERE question_number BETWEEN 1 AND 27;"
+            translation = 'SELECT type, hmong FROM translations;';
             break;
     }
-    console.log('language is: ', language);
 
-    // var language = req.query.language;
-    // language.toString();
-    // console.log('language', language)
     if (req.isAuthenticated()) {
+        var surveyObject = {};
         if (req.user.role == 'Resident') {
             // query db, get language selected
             pool.connect(function (err, client, done) {
@@ -38,13 +40,22 @@ router.get('/language', function (req, res) {
                     //query
                     client.query(language, function (err, data) {
                         done();
-                        // console.log('query result', data.rows)
                         if (err) {
                             console.log('query error', err);
                         } else {
-                            console.log('else')
+                            surveyObject.questions = data.rows;
+                            client.query(translation, function (err, data) {
+                                done();
+                                // console.log('query result', data.rows)
+                                if (err) {
+                                    console.log('query error', err);
+                                } else {
+                                    // send response data back to client
+                                    surveyObject.translations = data.rows;
+                                    res.send(surveyObject);
+                                }
+                            });
                             // send response data back to client
-                            res.send(data.rows);
                         }
                     });
                 }
