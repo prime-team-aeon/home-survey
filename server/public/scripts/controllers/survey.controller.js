ae159a021e06144afd2467fb958185f63a4906d6
Adam Biessener <abiessener@gmail.com>
2017-10-03 12:54:22 -0500

    input buttons functional

--- server/public/scripts/controllers/survey.controller.js
+++ server/public/scripts/controllers/survey.controller.js
@@ -1,17 +1,21 @@
-myApp.controller('SurveyController', function (SurveyService, $location) {
+myApp.controller('SurveyController', function (SurveyService, $location, $window) {
   console.log('SurveyController created');
   var self = this;
   // self.surveyObject = SurveyService.surveyObject;
   self.go = function (hash) {
     $location.path(hash);
+    $window.scrollTo(0,0);
   }
 
   self.getSurvey = function (language) {
     console.log('controller function ran with : ', language);
     SurveyService.getSurvey(language);
   }
-  
+
+  self.respond = function (question_id, answer) {
+    SurveyService.surveyAnswers.list[question_id-1].answer = answer;
+  }
+
+  self.surveyAnswers = SurveyService.surveyAnswers;
 
 });
-
-
--- server/public/scripts/services/survey.service.js
+++ server/public/scripts/services/survey.service.js
@@ -1,6 +1,9 @@
 myApp.service('SurveyService', function ($http, $location) {
     console.log('SurveyService Loaded');
 
+    const NUM_SURVEY_QUESTIONS = 27;
+    const SURVEY_TEXT_QUESTIONS = [21,22];
+    
     var self = this;
 
     self.surveyObject = { 
@@ -11,11 +14,20 @@ myApp.service('SurveyService', function ($http, $location) {
         demographics:[]
      };
 
-    self.surveyAnswers = [];
+    self.surveyAnswers = {list: []};
+
+    self.surveyLanguage = {language:"english"};
+
+    for (var i = 0; i < NUM_SURVEY_QUESTIONS; i++) {
+        // if(SURVEY_TEXT_QUESTIONS.indexOf(i+1) > 0){
+        //     self.surveyAnswers.list.push({answer: '...'})
+        // }
+        self.surveyAnswers.list.push({});
+    }
     
     self.getSurvey = function (language) {
         console.log('Service function ran with : ', language);
-        
+        self.surveyLanguage.language = language;
         $http.get('/survey/language', {params: { 'language': language}}).then(function (response) {
             console.log('Object from DB is: ', response.data);
             
