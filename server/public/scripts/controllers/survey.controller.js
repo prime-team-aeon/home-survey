79bb15b8dcbb86c9f92d70cd3766beb246706990
Ryan Templin <tempjuice@hotmail.com>
2017-10-03 10:14:21 -0500

    All 4 language selectors successfully query DB.

--- server/public/scripts/controllers/survey.controller.js
+++ server/public/scripts/controllers/survey.controller.js
@@ -1,13 +1,17 @@
-
-myApp.controller('SurveyController', ['SurveyService', '$location', function (SurveyService,$location) {
+myApp.controller('SurveyController', function (SurveyService, $location) {
   console.log('SurveyController created');
   var self = this;
-  self.surveyObject = SurveyService.surveyObject;
+  // self.surveyObject = SurveyService.surveyObject;
   self.go = function (hash) {
     $location.path(hash);
   }
+
+  self.getSurvey = function (language) {
+    console.log('controller function ran with : ', language);
+    SurveyService.getSurvey(language);
+  }
   
 
-}]);
+});
 
 
