458f3693943cda43a5232f113f7be71a4a156b30
Sam Fortin <sam.j.fortin@gmail.com>
2017-10-05 17:56:47 -0500

    Admin service and properties controller comments

--- server/public/scripts/controllers/admin-properties.controller.js
+++ server/public/scripts/controllers/admin-properties.controller.js
@@ -1,21 +1,34 @@
 myApp.controller('AdminPropertiesController', ['AdminService', '$mdDialog', function (AdminService, $mdDialog) {
+    
+    //--------------------------------------
+    //-------------VARIABLES----------------
+    //--------------------------------------
+    
     var self = this;
-
-    self.AdminService = AdminService;
-    AdminService.getAllProperties();
-
-    self.allProperties = AdminService.allProperties;
+    self.AdminService = AdminService; // connects AdminService to the AdminPropertiesController
+    self.allProperties = AdminService.allProperties; // list of all property information from the occupancy table
     self.uniqueProperties = AdminService.uniqueProperties;
 
-    // Send a new property function to the admin service
+    //--------------------------------------
+    //-------------FUNCTIONS----------------
+    //--------------------------------------
+
+    // Called from the Add Property button on the /admin-property page. Sends a nee property and unit number to the admin service
     self.addNewProperty = function () {
         AdminService.addNewProperty(AdminService.newProperty.name, AdminService.newProperty.unit);
     }
 
-    // Send an unit occupied status update to the admin service
-    self.updateOccupied = function(property) {
-        console.log('updateOccupied property', property);
+    // Called from a checkbox on the /admin-properties page. Sends unit occupied status update to the admin service
+    self.updateOccupied = function (property) {
         AdminService.updateOccupied(property);
     }
-    
-}]);
+
+    //--------------------------------------
+    //-------------RUNTIME CODE-------------
+    //--------------------------------------
+
+    // On controller load, get list of all property/unit combos from db
+    AdminService.getAllProperties();
+
+
+}]); // End of AdminPropertiesController
