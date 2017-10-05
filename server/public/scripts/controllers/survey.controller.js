6ae1c99cdc7f057c922cc87de81ec370559c0089
Adam Biessener <abiessener@gmail.com>
2017-10-05 11:57:01 -0500

    cleaned up and commented surveyController

--- server/public/scripts/controllers/survey.controller.js
+++ server/public/scripts/controllers/survey.controller.js
@@ -1,67 +1,20 @@
 myApp.controller('SurveyController', function (AdminService, SurveyService, $location, $window, $mdDialog) {
-  console.log('SurveyController created');
   var self = this;
-  // self.surveyObject = SurveyService.surveyObject;
-  self.go = function (hash) {
-    $location.path(hash);
-    $window.scrollTo(0, 0);
-  }
-  self.surveyObject = SurveyService.surveyObject;
 
-  // self.surveyObject=SurveyService.surveyObject;
-  self.getSurvey = function (language) {
-    console.log('controller function ran with : ', language);
-    SurveyService.getSurvey(language);
-  }
+  self.propertyChosen = ""; // the user-selected property
+  self.propertyList = AdminService.propertyList; // holds the list of properties pulled from the database
+  self.surveyAnswers = SurveyService.surveyAnswers; // holds the user's answers
+  self.surveyLanguage = SurveyService.surveyLanguage; // the user-selected language
+  self.surveyObject = SurveyService.surveyObject; // holds the translated questions for display
+  
 
-
-  self.surveyLanguage = SurveyService.surveyLanguage;
-
-  self.respond = function (question_id, answer) {
-    SurveyService.surveyAnswers.list[question_id - 1].answer = answer;
-  }
-
-  self.surveyAnswers = SurveyService.surveyAnswers;
-  self.help = function () {
-    var confirm = $mdDialog.confirm()
-      .title(self.surveyObject.instructions)
-      .textContent(self.surveyObject.directions1)
-      .ariaLabel(self.surveyObject.instructions)
-      .targetEvent(event)
-      .ok(self.surveyObject.continue);
-
-
-    $mdDialog.show(confirm).then(function () {
-      SurveyService.help();
-    }, function () {});
-
-  }
-
-
-
-  self.submitSurvey = function () {
-    var confirm = $mdDialog.confirm()
-      .title('Confirm Survey Submission')
-      .textContent('Do you want to submit your survey? This cannot be undone!')
-      .ariaLabel('confirm survey dialog')
-      .targetEvent(event)
-      .ok('Confirm')
-      .cancel('Cancel');
-
-    $mdDialog.show(confirm).then(function () {
-      SurveyService.submitSurvey();
-    }, function () {});
-
-  }
-
-  self.propertyList = AdminService.propertyList;
-
-  self.propertyChosen = "";
-
+  // passes the user-selected property and unit to the service to be checked against the db for whether or not it's a valid property and unit that has not yet responded to the survey
   self.beginSurvey = function (property, unit) {
     SurveyService.beginSurvey(property, unit);
   }
 
+
+  // displays a confirmation dialog for the user, and if confirmed clears the surveyAnswers object and sends the user back to the language-select page
   self.cancelSurvey = function () {
     var confirm = $mdDialog.confirm()
       .title('Confirm Cancel Survey')
@@ -75,6 +28,55 @@ myApp.controller('SurveyController', function (AdminService, SurveyService, $loc
       SurveyService.wipeSurveyClean();
       self.go('/survey-language');
     }, function () {});
+  }
 
+
+  // passes the user-selected language to the service so that surveyObject can be built with the translated questions
+  self.getSurvey = function (language) {
+    SurveyService.getSurvey(language);
+  }
+
+
+  // called primarily from prev/next buttons on DOM, sends user to the passed address and resets their scroll to the top of the page
+  self.go = function (hash) {
+    $location.path(hash);
+    $window.scrollTo(0, 0);
+  }
+
+  
+  // displays a dialog with translated help instructions for the user
+  self.help = function () {
+    var confirm = $mdDialog.confirm()
+      .title(self.surveyObject.instructions)
+      .textContent(self.surveyObject.directions1)
+      .ariaLabel(self.surveyObject.instructions)
+      .targetEvent(event)
+      .ok(self.surveyObject.continue);
+
+    $mdDialog.show(confirm).then(function () {
+      SurveyService.help();
+    }, function () {});
+  }
+
+
+  // takes hard-coded question_id and answer values from the user/DOM and puts them in surveyAnswers.list
+  self.respond = function (question_id, answer) {
+    SurveyService.surveyAnswers.list[question_id - 1].answer = answer;
+  }
+
+
+  // displays a confirmation dialog, and if confirmed invokes the service's submitSurvey function to store responses in the db
+  self.submitSurvey = function () {
+    var confirm = $mdDialog.confirm()
+      .title('Confirm Survey Submission')
+      .textContent('Do you want to submit your survey? This cannot be undone!')
+      .ariaLabel('confirm survey dialog')
+      .targetEvent(event)
+      .ok('Confirm')
+      .cancel('Cancel');
+
+    $mdDialog.show(confirm).then(function () {
+      SurveyService.submitSurvey();
+    }, function () {});
   }
 });
