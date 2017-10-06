bd5655f7b526b35284dc28f750a18016a7ad4ec9
Adam Biessener <abiessener@gmail.com>
2017-10-06 11:55:22 -0500

    sanitize inputs

--- server/routes/survey.router.js
+++ server/routes/survey.router.js
@@ -205,17 +205,13 @@ router.post('/', function (req, res) {
 
             var tableName = 'responses' + thisYear;
 
-            queryString = "INSERT INTO " + tableName + " (property, language, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, answer12, answer13, answer14, answer15, answer16, answer17, answer18, answer19, answer20, answer21, answer22, answer23, answer24, answer25, answer26, answer27) VALUES ('"
-
-            queryString += req.query.property + "', '" + req.query.language + "', '";
+            queryString = "INSERT INTO " + tableName + " (property, language, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, answer12, answer13, answer14, answer15, answer16, answer17, answer18, answer19, answer20, answer21, answer22, answer23, answer24, answer25, answer26, answer27) VALUES ($1, $2, '"
 
             for (var i = 0; i < req.body.list.length; i++) {
                 queryString += req.body.list[i].answer + "', '";
             }
 
             queryString = queryString.slice(0, -3) + ");";
-
-            // console.log('queryString:', queryString);
 
             pool.connect(function (err, client, done) {
                 if (err) {
@@ -267,7 +263,7 @@ router.post('/', function (req, res) {
                                                                                 console.log('connection err', err);
                                                                                 res.sendStatus(500);
                                                                             } else {
-                                                                                client.query(queryString, function (err, data) {
+                                                                                client.query(queryString, [req.query.property, req.query.language], function (err, data) {
                                                                                     done();
                                                                                     if (err) {
                                                                                         console.log('query error 258', err);
@@ -304,7 +300,7 @@ router.post('/', function (req, res) {
                                                                 console.log('connection err', err);
                                                                 res.sendStatus(500);
                                                             } else {
-                                                                client.query(queryString, function (err, data) {
+                                                                client.query(queryString, [req.query.property, req.query.language], function (err, data) {
                                                                     done();
                                                                     if (err) {
                                                                         console.log('query error', err);
