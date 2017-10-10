ad14054e46457e626d29892a503c32e5a2b4d235
Adam Biessener <abiessener@gmail.com>
2017-10-10 13:01:46 -0500

    fix export

--- server/routes/csv.router.js
+++ server/routes/csv.router.js
@@ -5,7 +5,7 @@ var path = require('path');
 var pool = require('../modules/pool.js');
 
 // Handles CSV upload from admin view
-router.post('/upload/:year', function (req, res) {  
+router.post('/upload/:year', function (req, res) {
 
   const OCCUPANCY_ROW_LENGTH = 3;
   const OCCUPANCY_IGNORE_ROWS = 1; // number of rows to ignore at top of imported .csv
@@ -17,25 +17,25 @@ router.post('/upload/:year', function (req, res) {
       if (req.user.role == 'Administrator') {
 
         // first, delete the old table
-        pool.connect(function (err,client,done){
-          if(err){
+        pool.connect(function (err, client, done) {
+          if (err) {
             console.log('connect error', err);
             res.sendStatus(500);
           } else {
-            client.query('DELETE FROM occupancy WHERE year=$1', [req.params.year], function(err,data){
+            client.query('DELETE FROM occupancy WHERE year=$1', [req.params.year], function (err, data) {
               done();
-              if(err){
+              if (err) {
                 console.log('delete query error', err);
                 res.sendStatus(500);
               } else {
                 var unitsArray = req.body.data;
                 var queryString = 'INSERT INTO occupancy (property, unit, year) VALUES (';
-        
+
                 // SQL query looks like:
                 // INSERT INTO occupancy (name, message) VALUES ($1, $2, $3), ($4,$5,$6), [...];
-        
+
                 var explodedArray = [];
-        
+
                 // assuming header row, push any valid row into the explodedArray and build out the query string
                 for (var i = OCCUPANCY_IGNORE_ROWS; i < unitsArray.length; i++) {
                   if (unitsArray[i].length === OCCUPANCY_ROW_LENGTH) {
@@ -47,10 +47,10 @@ router.post('/upload/:year', function (req, res) {
                     queryString += '),(';
                   }
                 }
-        
+
                 queryString = queryString.slice(0, -2);
                 queryString += ';';
-        
+
                 // push the whole thing into the occupancy table of the db
                 pool.connect(function (err, client, done) {
                   if (err) {
@@ -88,8 +88,6 @@ router.post('/upload/:year', function (req, res) {
 
 // exports all responses from the passed-in year. called from admin
 router.get('/export/:year', function (req, res) {
-  var tableName = 'responses' + req.params.year;
-
   if (req.isAuthenticated()) {
     if (req.user.role == 'Administrator') {
       // query db, get all responses
@@ -98,39 +96,22 @@ router.get('/export/:year', function (req, res) {
           console.log('error connecting to db', err);
           res.sendStatus(500);
         } else {
-          // get table names
-          client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';", function (err, data) {
+          var queryString = 'SELECT * FROM responses WHERE year=$1';
+
+          // run the actual query
+          client.query(queryString, [req.params.year], function (err, data) {
+            done();
             if (err) {
-              done();
               console.log('query error', err);
             } else {
-              // check that this table exists
-              var foundTable = data.rows.find(function (table) {
-                return table.table_name == tableName;
-              });
-
-              // for some reason doing the usual $1 and passing [tablename] gave query errors. magnets, how do they work?
-              var queryString = 'SELECT * FROM ' + tableName + ';';
-
-              if (foundTable != undefined) {
-                // run the actual query
-                client.query(queryString, function (err, data) {
-                  done();
-                  if (err) {
-                    console.log('query error', err);
-                  } else {
-                    // send response data back to client
-                    res.send(data.rows);
-                  }
-                });
-              } else {
-                done();
-                res.status(400).send('no data for year ' + req.params.year);
-              }
+              // send response data back to client
+              res.send(data.rows);
             }
           });
+
         }
       });
+
     } else {
       // not admin role
       res.sendStatus(403);
